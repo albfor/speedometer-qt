b756c36e1303828432e737446ad079591d123551
Albin Forsberg <96afo02@gmail.com>
2024-09-16 20:37:26 +0200

    organized variables in canvas with namespaces

--- desktop/client/include/canvas.h
+++ desktop/client/include/canvas.h
@@ -4,101 +4,103 @@
 #include "comservice.h"
 
 #include <QColor>
+#include <QFont>
 #include <QPainter>
 #include <QWidget>
-#include <qfont.h>
+#include <qcontainerfwd.h>
 
-// Arc rectangle dimensions and angles used for drawing an arc
-constexpr QRect ARC_RECTANGLE(70, 50, 600, 600); // Rectangle defining the boundary for the arc
-constexpr int START_ANGLE = 330 * 16;            // Starting angle of the arc in 1/16th of a degree
-constexpr int SPAN_ANGLE = 240 * 16;             // Span angle of the arc in 1/16th of a degree
+namespace speedometer
+{
+constexpr QRect arc_rectangle(70, 50, 600, 600); // rectangle defining the boundary for the arc
+constexpr int start_angle = 330 * 16;            // starting angle of the arc in 1/16th of a degree
+constexpr int span_angle = 240 * 16;             // span angle of the arc in 1/16th of a degree
 
-// Tick marks on the arc
-constexpr int NUM_LARGE_TICKS = 13;                    // Number of large tick marks (major divisions) on the arc
-constexpr int TOTAL_TICKS = (NUM_LARGE_TICKS - 1) * 4; // Total number of ticks, including smaller ones
-constexpr int ANGLE_INCREMENT = 240 / TOTAL_TICKS;     // Angle between each tick mark
+namespace tick
+{
+constexpr int small_width = 2;
+constexpr int medium_width = 4;
+constexpr int large_width = 6;
+constexpr int start = 250;
+constexpr int small_end = 270;
+constexpr int medium_end = 275;
+constexpr int large_end = 280;
+constexpr int label = 21;
+constexpr int label_offset_x = -15;
+constexpr int label_offset_y = 9;
+constexpr int number_of_large_ticks = 13;
+constexpr int total_ticks = (number_of_large_ticks - 1) * 4;
+constexpr int angle_increment = 240 / total_ticks;
+constexpr int initial_angle = 330;
+constexpr int large_interval = 4;
+constexpr int medium_interval = 2;
+} // namespace tick
 
-// Initial angle and tick mark lengths
-constexpr int INITIAL_ANGLE = 330; // Starting angle for the first tick mark in degrees
-
-// Text positioning relative to tick marks
-constexpr int LABEL_TO_TICK_DISTANCE = 21;  // Offset distance for the text from the tick marks in pixels
-constexpr int LABEL_TO_TICK_OFFSET_X = -15; // X-axis offset for text positioning relative to tick marks
-constexpr int LABEL_TO_TICK_OFFSET_Y = 9;   // Y-axis offset for text positioning relative to tick marks
-
-// Needle value and offset
-constexpr int NEEDLE_LENGTH = 220;
-constexpr int NEEDLE_OFFSET = 210;
-const QFont HELVETICA_FONT_22("Helvetica", 22);
-
-// Magic numbers turned into constants for clarity
-constexpr int ELLIPSE_RADIUS = 18; // Radius of the central ellipse in pixels
-const QPoint SPEED_LABEL_POSITION(345, 450);
-const QPoint ERROR_LABEL_POSITION(300, 450);
-
-// Battery
-const QPoint BATTERY_LEVEL_POSITION(725, 360);
-const QSize BATTERY_LEVEL_SIZE(35, -80);
-
-const QFont HELVETICA_FONT_14("Helvetica", 14);
-const QFont HELVETICA_FONT_10("Helvetica", 10);
-
-// Declaration of the Canvas class, which inherits from QWidget
-class Canvas : public QWidget
+namespace icons
 {
 
+struct Icon
+{
+    const QPoint position;
+    const QChar icon;
+    const QFont font;
+};
+
+struct IconWithColoredStates : Icon
+{
+    const int low, high;
+    const QColor lowc, medc, highc;
+    const QPoint label;
+    const QFont label_font;
+};
+
+const QString icon_font("MaterialIcons");
+const QString text_font("Helvetica");
+const Icon left_turn{QPoint(50, 75), QChar(0xe5c4), QFont(icon_font, 50)};
+const Icon right_turn{QPoint(600, 75), QChar(0xe5c8), QFont(icon_font, 50)};
+const Icon connection_ok{QPoint(345, 430), QChar(0xe9e4), QFont(icon_font, 40)};
+const Icon connection_error{QPoint(345, 430), QChar(0xe628), QFont(icon_font, 40)};
+constexpr QPoint speed_label(345, 450);
+constexpr QPoint errror_label(300, 450);
+const IconWithColoredStates battery{{QPoint(675, 380), QChar(0xebdc), QFont(icon_font, 100)},
+                                    25,
+                                    50,
+                                    Qt::red,
+                                    Qt::yellow,
+                                    Qt::green,
+                                    QPoint(730, 385),
+                                    QFont(text_font, 10)};
+constexpr QPoint battery_level(725, 360);
+constexpr QSize battery_level_size(35, -80);
+const IconWithColoredStates temperature{{QPoint(710, 480), QChar(0xe1ff), QFont(icon_font, 50)},
+                                        5,
+                                        40,
+                                        Qt::white,
+                                        Qt::blue,
+                                        Qt::red,
+                                        QPoint(730, 490),
+                                        QFont(text_font, 10)};
+
+} // namespace icons
+
+namespace needle
+{
+constexpr int length = 220;
+constexpr int offset = 210;
+constexpr QPoint attach_point = arc_rectangle.center();
+constexpr int attach_point_size = 18; // radius of the central ellipse in pixels
+} // namespace needle
+} // namespace speedometer
+
+class Canvas : public QWidget
+{
 public:
-    // Constructor declaration for Canvas class
     explicit Canvas(COMService *comserv, QWidget *parent = nullptr);
     bool is_visible = true;
 
 protected:
-    // Override of the paintEvent function to handle custom painting
     void paintEvent(QPaintEvent *event) override;
 
 private:
-    static const QString icon_font;
-    static const QString text_font;
-    struct Icon
-    {
-        const QPoint position;
-        const QChar icon;
-        const QFont font;
-    };
-
-    struct IconWithColoredStates : Icon
-    {
-        const int low, high;
-        const QColor lowc, medc, highc;
-        const QPoint label;
-        static const QFont label_font;
-    };
-
-    struct TickSettings
-    {
-        int small_width, small_len, medium_width, medium_len, large_width, large_len;
-    };
-
-    const Icon left_turn_signal{QPoint(50, 75), QChar(0xe5c4), QFont(icon_font, 50)};
-    const Icon right_turn_signal{QPoint(600, 75), QChar(0xe5c8), QFont(icon_font, 50)};
-    const Icon connection_ok{QPoint(345, 430), QChar(0xe9e4), QFont(icon_font, 40)};
-    const Icon connection_error{QPoint(345, 430), QChar(0xe628), QFont(icon_font, 40)};
-    const IconWithColoredStates battery{
-        {QPoint(675, 380), QChar(0xebdc), QFont(icon_font, 100)},
-        25,
-        50,
-        Qt::red,
-        Qt::yellow,
-        Qt::green,
-        QPoint(730, 385),
-    };
-    const IconWithColoredStates temperature{
-        {QPoint(710, 480), QChar(0xe1ff), QFont(icon_font, 50)}, 5, 40, Qt::white, Qt::blue, Qt::red, QPoint(730, 490),
-    };
-
-    static constexpr TickSettings tick_settings{2, 270, 4, 275, 6, 280};
-    static constexpr int tick_to_center_distance = 250;
-
     COMService *comserv;
     QColor backgroundColor;
 
@@ -106,10 +108,8 @@ private:
     void paint_turn_signals(QPainter &painter);
     void paint_ticks(QPainter &painter, QPen &pen);
     void paint_temperature(QPainter &painter);
-    QColor determine_color(IconWithColoredStates icon, int value);
-    void manage_blinker_sound();
-    void paint_icon(QPainter &painter, const Icon &icon);
+    QColor determine_color(const speedometer::icons::IconWithColoredStates &icon, int value);
+    void paint_icon(QPainter &painter, const speedometer::icons::Icon &icon);
 };
-
 
 #endif // CANVAS_H
--- desktop/client/src/canvas.cpp
+++ desktop/client/src/canvas.cpp
@@ -4,18 +4,15 @@
 #include <QDebug>
 #include <QDir>
 #include <QFileInfo>
+#include <qmath.h>
 
 Canvas::Canvas(COMService *comserv, QWidget *parent) : QWidget(parent), comserv(comserv), backgroundColor(Qt::black)
 {
 }
 
-const QString Canvas::text_font("Helvetica");
-const QString Canvas::icon_font("MaterialIcons");
-const QFont Canvas::IconWithColoredStates::label_font(text_font, 10);
-
-// Override of the paintEvent method to perform custom painting
 void Canvas::paintEvent(QPaintEvent *event)
 {
+    using namespace speedometer;
     QWidget::paintEvent(event);
     QPainter painter(this);
 
@@ -26,40 +23,39 @@ void Canvas::paintEvent(QPaintEvent *event)
     QPen pen(Qt::white, 10, Qt::SolidLine, Qt::FlatCap, Qt::RoundJoin);
     painter.setPen(pen);
 
-    // Draw an arc using the defined pen and constants
-    painter.drawArc(ARC_RECTANGLE, START_ANGLE, SPAN_ANGLE);
+    // outer edge speedometer
+    painter.drawArc(arc_rectangle, start_angle, span_angle);
 
     // Calculate the center of the arc rectangle
-    QPoint center = ARC_RECTANGLE.center();
+    QPoint needle_attach_point = needle::attach_point;
 
     // Set brush color to red and draw a circle at the center of the arc
     painter.setBrush(Qt::red);
-    painter.drawEllipse(center, ELLIPSE_RADIUS, ELLIPSE_RADIUS);
+    painter.drawEllipse(needle_attach_point, needle::attach_point_size, needle::attach_point_size);
 
     paint_ticks(painter, pen);
     paint_temperature(painter);
     paint_battery(painter);
 
-    // Draw connection status icon and text based on the connection status
-    auto paint_connection = [&painter, this](Icon icon, QString message, QPoint label_position) {
+    // paint connection status icon and text based on the connection status
+    auto paint_connection = [&painter, this](icons::Icon icon, QString message, QPoint label_position) {
         paint_icon(painter, icon);
-        painter.setFont(HELVETICA_FONT_14);
+        painter.setFont(QFont(speedometer::icons::text_font, 14));
         painter.drawText(label_position, message);
     };
     if (comserv->is_connected())
-        paint_connection(connection_ok, QString::number(comserv->get_speed()) + " km/h", SPEED_LABEL_POSITION);
+        paint_connection(icons::connection_ok, QString("%1 km/h").arg(comserv->get_speed()), icons::speed_label);
     else
-        paint_connection(connection_error, "Connection Error!", ERROR_LABEL_POSITION);
+        paint_connection(icons::connection_error, "Connection Error!", icons::errror_label);
 
-    // Draw the needle
-    painter.setFont(HELVETICA_FONT_22);
+    // paint the needle
     pen.setCapStyle(Qt::RoundCap);
     pen.setWidth(10);
     pen.setColor(Qt::red);
     painter.setPen(pen);
-    qreal needle_radians = qDegreesToRadians(NEEDLE_OFFSET - comserv->get_speed());
-    painter.drawLine(center, QPointF(center.x() + NEEDLE_LENGTH * qCos(needle_radians),
-                                     center.y() - NEEDLE_LENGTH * qSin(needle_radians)));
+    qreal needle_radians = qDegreesToRadians(needle::offset - comserv->get_speed());
+    painter.drawLine(needle_attach_point, QPointF(needle_attach_point.x() + needle::length * qCos(needle_radians),
+                                                  needle_attach_point.y() - needle::length * qSin(needle_radians)));
 
     paint_turn_signals(painter);
 }
@@ -100,50 +96,52 @@ void Canvas::paint_turn_signals(QPainter &painter)
 
         // draw the icons
         painter.setPen(left);
-        paint_icon(painter, left_turn_signal);
+        paint_icon(painter, speedometer::icons::left_turn);
         painter.setPen(right);
-        paint_icon(painter, right_turn_signal);
+        paint_icon(painter, speedometer::icons::right_turn);
     }
 }
 
 void Canvas::paint_ticks(QPainter &painter, QPen &pen)
 {
-    for (int i = 0; i <= TOTAL_TICKS; i++)
+    using namespace speedometer::tick;
+    using speedometer::needle::attach_point;
+
+    for (int tick = 0; tick <= total_ticks; tick++)
     {
-        // Calculate the angle for the tick mark
-        int angle = INITIAL_ANGLE + i * ANGLE_INCREMENT;
-        qreal radians = qDegreesToRadians(angle); // Convert angle to radians
-        qreal cosRadians = qCos(radians);
-        qreal sinRadians = qSin(radians);
-        QPoint center = ARC_RECTANGLE.center();
-        QPoint tick_start(center.x() + tick_to_center_distance * cosRadians,
-                          center.y() - tick_to_center_distance * sinRadians);
+        // calculate the angle for the tick mark
+        int angle = initial_angle + tick * angle_increment;
+        qreal radians = qDegreesToRadians(angle); // convert angle to radians
+        qreal cos = qCos(radians);
+        qreal sin = qSin(radians);
+        QPoint tick_start(attach_point.x() + start * cos, attach_point.y() - start * sin);
         QPoint tick_end;
 
-        auto set_tick_width_and_end = [&pen, &tick_end, &center, cosRadians, sinRadians](int width, int length) {
+        auto set_tick_width_and_endpoint = [&pen, &tick_end, cos, sin](int width, int distance_from_center) {
             pen.setWidth(width);
-            tick_end.setX(center.x() + length * cosRadians);
-            tick_end.setY(center.y() - length * sinRadians);
+            tick_end.setX(attach_point.x() + distance_from_center * cos);
+            tick_end.setY(attach_point.y() - distance_from_center * sin);
         };
 
-        if (i % 4 == 0) // large ticks
+        // determine tick size
+        if (tick % large_interval == 0)
         {
-            set_tick_width_and_end(tick_settings.large_width, tick_settings.large_len);
-
-            // Calculate and display the speed label
-            int speed_label = (TOTAL_TICKS - i) / 4 * 20;
-            QPoint speed_label_position(tick_start.x() - LABEL_TO_TICK_DISTANCE * cosRadians + LABEL_TO_TICK_OFFSET_X,
-                                        tick_start.y() + LABEL_TO_TICK_DISTANCE * sinRadians + LABEL_TO_TICK_OFFSET_Y);
+            set_tick_width_and_endpoint(large_width, large_end);
+            // calculate and display the speed label
+            int speed_label = (total_ticks - tick) / 4 * 20;
+            QPoint speed_label_position(tick_start.x() - label * cos + label_offset_x,
+                                        tick_start.y() + label * sin + label_offset_y);
             painter.drawText(speed_label_position, QString::number(speed_label));
         }
-        else if (i % 4 == 2) // medium ticks
+        else if (tick % medium_interval == 0)
         {
-            set_tick_width_and_end(tick_settings.medium_width, tick_settings.medium_len);
+            set_tick_width_and_endpoint(medium_width, medium_end);
         }
-        else // small ticks
+        else
         {
-            set_tick_width_and_end(tick_settings.small_width, tick_settings.small_len);
+            set_tick_width_and_endpoint(small_width, small_end);
         }
+
         painter.setPen(pen);
         painter.drawLine(tick_start, tick_end);
     }
@@ -151,6 +149,7 @@ void Canvas::paint_ticks(QPainter &painter, QPen &pen)
 
 void Canvas::paint_temperature(QPainter &painter)
 {
+    using speedometer::icons::temperature;
     int temp = comserv->get_temperature();
     painter.setPen(determine_color(temperature, temp));
     paint_icon(painter, temperature);
@@ -161,27 +160,31 @@ void Canvas::paint_temperature(QPainter &painter)
 
 void Canvas::paint_battery(QPainter &painter)
 {
+    using speedometer::icons::battery;
     unsigned battery_level = comserv->get_battery_level();
     QColor battery_color = determine_color(battery, battery_level);
     painter.setPen(battery_color);
     painter.setBrush(battery_color);
     paint_icon(painter, battery);
-    QRect battery_level_bar(BATTERY_LEVEL_POSITION, BATTERY_LEVEL_SIZE); // bar inside icon showing percentage
+    // bar inside icon showing percentage
+    QRect battery_level_bar(speedometer::icons::battery_level, speedometer::icons::battery_level_size);
+
     // map battery_level to gui
-    battery_level_bar.setHeight((BATTERY_LEVEL_SIZE.height() * static_cast<int>(battery_level)) / 100);
+    battery_level_bar.setHeight((speedometer::icons::battery_level_size.height() * static_cast<int>(battery_level)) /
+                                100);
     painter.drawRect(battery_level_bar);
     painter.setPen(Qt::white);
     painter.setFont(battery.label_font);
     painter.drawText(battery.label, QString("%1%").arg(battery_level, 3)); // 3 is max digits
 }
 
-void Canvas::paint_icon(QPainter &painter, const Icon &icon)
+void Canvas::paint_icon(QPainter &painter, const speedometer::icons::Icon &icon)
 {
     painter.setFont(icon.font);
     painter.drawText(icon.position, icon.icon);
 }
 
-QColor Canvas::determine_color(IconWithColoredStates icon, int value)
+QColor Canvas::determine_color(const speedometer::icons::IconWithColoredStates &icon, int value)
 {
     return value < icon.low ? icon.lowc : value < icon.high ? icon.medc : icon.highc;
 }
